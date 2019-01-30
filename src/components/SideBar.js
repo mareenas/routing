5418e12bfbb7cbabb848c57f63a03c4dd45ba854
mareenas <persempre1@yandex.ru>
2019-01-30 17:00:32 +0300

    navlink styling

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,12 +1,13 @@
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import React from 'react';
+import style from './css/Sidebar.module.css';
 
 const SideBar = () => {
     return(
         <div>
             <ul>
-                <Link to="/profile"><li>Профиль</li></Link>
-                <Link to="/dialogs"><li>Сообщения</li></Link>
+                <NavLink activeClassName={style.activeLink} to="/profile"><li>Профиль</li></NavLink>
+                <NavLink activeClassName={style.activeLink} to="/dialogs"><li>Сообщения</li></NavLink>
                 <li>Новости</li>
                 <li>Музыка</li>
                 <li>Настройки</li>
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -3,10 +3,11 @@ import {SET_LOG_IN_TO_FALSE, SET_LOG_IN_TO_TRUE} from "../actions";
 
 let initialState = {
     isAuth: false,
-    status: '',
-    errors: '',
-    message: '',
-    captchaUrl: ''
+    userInfo: {
+        userId: null,
+        userName: '',
+        avatarUrl: "https://marketplace.canva.com/MAB6v7RGMOw/1/thumbnail/canva-robot-electric-avatar-icon-MAB6v7RGMOw.png"
+    }
 };
 
 export const authReducer = (state = initialState, action) => {
